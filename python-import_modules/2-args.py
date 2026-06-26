d609e86e0caec866a658fe257c8d76948bc0fb06
akagermain <g.iradukund2@alustudent.com>
2026-06-26 13:34:41 +0200

    Add file two for task two, arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+argc = len(sys.argv) - 1
+
+if argc == 0:
+    print("0 arguments.")
+elif argc == 1:
+    print("1 argument:")
+else:
+    print("{:d} arguments:".format(argc))
+
+for i in range(1, len(sys.argv)):
+    print("{:d}: {:s}".format(i, sys.argv[i]))
