fc856777c73bcc5b4b59b28471cf0bd58ae6ead9
akagermain <g.iradukund2@alustudent.com>
2026-06-26 14:17:27 +0200

    Modify file two, fixing errors

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,5 @@ if __name__ = "__main__":
         print("1 argument:")
     else:
         print("{:d} arguments:".format(argc))
-
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
