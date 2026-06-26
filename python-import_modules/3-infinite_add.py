4c7b3fd757d75cdeac70a7888f7ebde2904f4e4e
akagermain <g.iradukund2@alustudent.com>
2026-06-26 14:24:51 +0200

    File three, error fixation

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-result = 0
-for i in range(1, len(sys.argv)):
-    result += int(sys.argv[i])
-
-print(result)
+if __name__ == "__main__":
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print(result)
