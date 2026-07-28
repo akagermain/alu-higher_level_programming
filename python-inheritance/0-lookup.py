415bb19d106e477387488bf704e0ebf51e2969a2
akagermain <g.iradukund2@alustudent.com>
2026-07-28 09:33:35 +0200

    Modify file zero, lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""This module provides a function to list an object's available attributes and methods."""
+"""This module provides a function to list an object's available attributes
+and methods.
+"""
 
 
 def lookup(obj):
-    """Return a list containing the attributes and methods available in an object."""
+    """Return a list containing the attributes and methods available in
+    an object.
+    """
     return dir(obj)
