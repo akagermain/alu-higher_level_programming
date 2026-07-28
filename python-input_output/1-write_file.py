bf3cd4c05dca742b2489b5b3c655b82a4c7f78ff
akagermain <g.iradukund2@alustudent.com>
2026-07-28 10:42:23 +0200

    Add file one, write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module provides a function for writing text to a UTF-8 file."""
+
+
+def write_file(filename="", text=""):
+    """Write text to a UTF-8 file and return the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
