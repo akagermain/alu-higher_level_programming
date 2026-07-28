1a4100f5baeceff4c6f824e2ff95795266a1cbe2
akagermain <g.iradukund2@alustudent.com>
2026-07-28 10:45:42 +0200

    Modify file one

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """Write text to a UTF-8 file and return the number of characters written."""
+    """Write text to a UTF-8 file and return the number of characters
+    written.
+    """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
