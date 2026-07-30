bfd9c24341e95fdb0de6889e225a52743b576ef1
akagermain <g.iradukund2@alustudent.com>
2026-07-30 09:44:27 +0000

    Add file one, root user

---
+++ SQL_more_queries/1-create_user_.sql
@@ -0,0 +1,9 @@
+-- Creates the MySQL user user_0d_1 with all privileges
+
+-- Create the user if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION;
