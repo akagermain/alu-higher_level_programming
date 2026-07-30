711da10e29a61a021bc7543b19560c05a72d5b13
akagermain <g.iradukund2@alustudent.com>
2026-07-30 08:58:29 +0000

    Add file ten, list by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,13 @@
+-- Lists all records from second_table ordered by score in descending order
+
+-- Retrieve the score and name of all records ordered by score
+SELECT score, name FROM second_table
+ORDER BY score DESC;1~-- Lists all records from second_table ordered by score in descending order
+
+-- Retrieve the score and name of all records ordered by score
+SELECT score, name FROM second_table
+ORDER BY score DESC;1~m-- Lists all records from second_table ordered by score in descending order
+
+-- Retrieve the score and name of all records ordered by score
+SELECT score, name FROM second_table
+ORDER BY score DESC;
