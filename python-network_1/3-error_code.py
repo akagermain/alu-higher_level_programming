42f5c9b9d1ec97d4aca949a8f1490129f5983593
akagermain <g.iradukund2@alustudent.com>
2026-07-31 08:00:27 +0000

    File three modfiy

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Sends a request to a URL and displays the response or the HTTP error code."""
+"""Sends a request to a URL and prints the body or HTTP error code."""
 from urllib import error, request
 import sys
-try:
-    with request.urlopen(sys.argv[1]) as response:
-        print(response.read().decode("utf-8"))
-except error.HTTPError as err:
-    print("Error code: {}".format(err.code))
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as e:
+        print("Error code:", e.code)
