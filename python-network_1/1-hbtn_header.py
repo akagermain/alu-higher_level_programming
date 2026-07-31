7e835cd95cc895cb84d1008685b224efe1b18f6c
akagermain <g.iradukund2@alustudent.com>
2026-07-31 07:47:23 +0000

    Modify file 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a request to a URL and displays the X-Request-Id response header."""
+"""Sends a request to a URL, displays the X-Request-Id response header."""
 from urllib import request
 import sys
 with request.urlopen(sys.argv[1]) as response:
